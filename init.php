0d455061cbdb9cf31471118e03bc362220722e0b
Glenn Ansley <glenn@ithemes.com>
2014-05-23 10:38:21 -0500

    Finish backend functionaltiy.

--- init.php
+++ init.php
@@ -13,14 +13,12 @@
  * @return void
 */
 function it_exchange_modify_default_redirects_settings_callback() {
-	$form_values  = it_exchange_get_option( 'addon_modify_default_redirects', true );
-	if ( ! isset( $form_values['show-continue-shopping-button'] ) )
-		$form_values['show-continue-shopping-button'] = true;
+	$settings = it_exchange_get_option( 'addon_modify_default_redirects', true );
 	$form_options = array(
 		'id'      => 'it-exchange-add-on-modify-default-redirects-settings',
 		'action'  => 'admin.php?page=it-exchange-addons&add-on-settings=modify-default-redirects',
 	);
-	$form         = new ITForm( $form_values, array( 'prefix' => 'it-exchange-add-on-modify-default-redirects' ) );
+	$form         = new ITForm( array(), array( 'prefix' => 'it-exchange-add-on-modify-default-redirects' ) );
 
 	// Get our whitelist for approved actions
 	$possible_redirects = it_exchange_addon_modify_default_redirects_get_registered_redirects();
@@ -35,31 +33,9 @@ function it_exchange_modify_default_redirects_settings_callback() {
 
 		<?php
 		do_action( 'it_exchange_digital_downloads_settings_form_top' );
-		if ( ! empty( $form_values ) )
-			foreach ( $form_values as $key => $var )
-				$form->set_option( $key, $var );
-
 		if ( ! empty( $_POST['__it-form-prefix'] ) && 'it-exchange-add-on-modify-default-redirects' == $_POST['__it-form-prefix'] )
 			ITUtility::show_status_message( __( 'Options Saved', 'LION' ) );
 
-		// Loop through saved settings and print them out
-		/*
-		$action_select = '
-		<select id="it-exchange-addon-modify-default-redirects-possible-rewrites-select">
-			<option value="">' . __( 'Select an action', 'LION' ) . '</option>';
-			foreach( $possible_redirects as $slug => $redirect ) {
-				$action_select .= '<option value="' . esc_attr( $slug ) . '">' . $redirect['select_title'] . '</select>';
-			}
-		$action_select .= '</select>';
-		*/
-		$action_target_type = '
-		<select id="it-exchange-addon-modify-default-redirects-target-types">
-		<option value="0">' . __( 'Default', 'LION' ) . '</option>
-		<option value="exchange-page">' . __( 'Exchange Page', 'LION' ) . '</option>
-		<option value="wp-page">' . __( 'WordPress Page', 'LION' ) . '</option>
-		<option value="wp-post">' . __( 'WordPress Post', 'LION' ) . '</option>
-		<option value="external-url">' . __( 'External URL', 'LION' ) . '</option>
-		</select>';
 		?>
 		<div class="it-exchange-addon-modify-default-redirects-table">
 			<div class="it-row ps-header">
@@ -74,16 +50,32 @@ function it_exchange_modify_default_redirects_settings_callback() {
 				</div>
 			</div>
 			<?php foreach( $possible_redirects as $slug => $redirect ) {
+				$selected_type = empty( $settings[$slug]['type'] ) ? 'default' : $settings[$slug]['type'];
+				$selected      = empty( $settings[$slug]['target'] ) ? 'default' : $settings[$slug]['target'];
 				?>
 				<div class="it-row">
 					<div class="it-column column-1">
 						<span><?php echo $redirect['setting_title']; ?></span><?php if ( ! empty( $redirect['tooltip'] ) ) { it_exchange_admin_tooltip( $redirect['tooltip'] ); } ?>
 					</div>
 					<div class="it-column column-2">
-						<span><?php echo $action_target_type; ?></span>
+						<span><?php echo it_exchange_modify_default_redirects_get_settings_dropdown( 'type-selector', $slug, $selected_type ); ?></span>
 					</div>
 					<div class="it-column column-3">
-						<span><?php echo $redirect['defaults_to']; ?></span>
+						<div class="landing-page default <?php echo ( 'default' == $selected_type ) ? '' : 'hide-if-js'; ?>">
+							<span><?php echo $redirect['defaults_to']; ?></span>
+						</div>
+						<div class="landing-page exchange <?php echo ( 'exchange' == $selected_type ) ? '' : 'hide-if-js'; ?>">
+							<span><?php echo it_exchange_modify_default_redirects_get_settings_dropdown( 'exchange', $slug, $selected ); ?></span>
+						</div>
+						<div class="landing-page wp-page <?php echo ( 'wp-page' == $selected_type ) ? '' : 'hide-if-js'; ?>">
+							<span><?php echo it_exchange_modify_default_redirects_get_settings_dropdown( 'wp-page', $slug, $selected ); ?></span>
+						</div>
+						<div class="landing-page wp-post <?php echo ( 'wp-post' == $selected_type ) ? '' : 'hide-if-js'; ?>">
+							<span><?php echo it_exchange_modify_default_redirects_get_settings_dropdown( 'wp-post', $slug, $selected ); ?></span>
+						</div>
+						<div class="landing-page external-url <?php echo ( 'external-url' == $selected_type ) ? '' : 'hide-if-js'; ?>">
+							<span><input type="text" name="it-exchange-modify-default-redirects[<?php echo $slug; ?>][external-url]" value="<?php esc_attr_e( $selected ); ?>" placeholder="<?php esc_attr_e( 'http://' ); ?>" /></span>
+						</div>
 					</div>
 				</div>
 				<?php
@@ -157,23 +149,6 @@ function it_exchange_addon_modify_default_redirects_get_registered_redirects() {
 }
 
 /**
- * Saves the settings page values
- *
- * @since 1.0.0
- *
- * @return void
-*/
-function it_exchange_modify_default_redirects_save_settings() {
-	if ( empty( $_POST['__it-form-prefix'] ) || 'it-exchange-add-on-modify-default-redirects' != $_POST['__it-form-prefix'] )
-		return;
-
-	$form_values = it_exchange_get_option( 'addon_modify_default_redirects', true );
-	$form_values['show-continue-shopping-button'] = ! empty( $_POST['it-exchange-add-on-modify-default-redirects-show-continue-shopping-button'] );
-	it_exchange_save_option( 'addon_modify_default_redirects', $form_values );
-}
-add_action( 'admin_init', 'it_exchange_modify_default_redirects_save_settings' );
-
-/**
  * Enqueues css and js for admin page
  *
  * @since 1.0.0
@@ -188,6 +163,90 @@ function it_exchange_addon_modify_default_redirects_enqueue_admin_scripts( $pref
 		return;
 
 	wp_enqueue_script( 'it-exchange-dialog' );
+	wp_enqueue_script( 'it-exchange-addon-modify-default-redirects-admin', ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/assets/admin.js' ) );
 	wp_enqueue_style( 'it-exchange-addon-modify-default-redirects-admin', ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/assets/admin.css' ) );
 }
 add_action( 'admin_enqueue_scripts', 'it_exchange_addon_modify_default_redirects_enqueue_admin_scripts', 11 );
+
+/**
+ * Returns a select box for the settings page
+ *
+ * @since 1.0.0
+ *
+ * @param  string $type     the type of redirect. eg: default, exchange, wp-post, wp-page, external-url
+ * @param  string $slug     the identifier
+ * @param  mixed  $selected the selected value
+ * @return string           the select field
+*/
+function it_exchange_modify_default_redirects_get_settings_dropdown( $type, $slug, $selected ) {
+	switch( $type ) {
+		case 'exchange' :
+			$pages   = it_exchange_get_pages();
+			$select  = '<select name="it-exchange-modify-default-redirects[' . $slug . '][exchange]">';
+			$select .= '<option value="0" ' . selected( $selected, false, false ) . '>Select an Exchange page</option>';
+			foreach( (array) $pages as $page ) {
+				if ( empty( $page['menu'] ) )
+					continue;
+				$select .= '<option value="' . $page['slug'] . '" ' . selected( $selected, $page['slug'], false ) . '>' . $page['name'] . '</option>';
+			}
+			$select .= '</select>';
+			return $select;
+			break;
+		case 'wp-page' :
+			$pages = get_pages();
+			$select  = '<select name="it-exchange-modify-default-redirects[' . $slug . '][wp-page]">';
+			$select .= '<option value="0" ' . selected( $selected, false, false ) . '>' . __( 'Select a WordPress Page', 'LION' ) . '</option>';
+			foreach( (array) $pages as $page ) {
+				$select .= '<option value="' . $page->ID . '" ' . selected( $selected, $page->ID, false ) . '>' . get_the_title( $page->ID ) . '</option>';
+			}
+			$select .= '</select>';
+			return $select;
+			break;
+		case 'wp-post' :
+			$posts   = get_posts();
+			$select  = '<select name="it-exchange-modify-default-redirects[' . $slug . '][wp-post]">';
+			$select .= '<option value="0" ' . selected( $selected, false, false ) . '>' . __( 'Select a WordPress Post', 'LION' ) . '</option>';
+			foreach( (array) $posts as $post ) {
+				$select .= '<option value="' . $post->ID . '" ' . selected( $selected, $post->ID, false ) . '>' . get_the_title( $post->ID ) . '</option>';
+			}
+			$select .= '</select>';
+			return $select;
+			break;
+		case 'type-selector' :
+			$select  = '<select class="it-exchange-addon-modify-default-redirects-target-types" name="it-exchange-modify-default-redirects[' . $slug . '][type]">';
+			$select .= '<option value="default" ' . selected( 'default', $selected, false ) . '>' . __( 'Default', 'LION' ) . '</option>';
+			$select .= '<option value="exchange" ' . selected( 'exchange', $selected, false ) . '>' . __( 'Exchange Page', 'LION' ) . '</option>';
+			$select .= '<option value="wp-page" ' . selected( 'wp-page', $selected, false ) . '>' . __( 'WordPress Page', 'LION' ) . '</option>';
+			$select .= '<option value="wp-post" ' . selected( 'wp-post', $selected, false ) . '>' . __( 'WordPress Post', 'LION' ) . '</option>';
+			$select .= '<option value="external-url" ' . selected( 'external-url', $selected, false ) . '>' . __( 'External URL', 'LION' ) . '</option>';
+			$select .= '</select>';
+			return $select;
+			break;
+	}
+}
+
+/**
+ * Saves the settings
+ *
+ * @since 1.0.0
+ *
+ * @return void
+*/
+function it_exchange_modify_default_redirects_save_settings() {
+    if ( empty( $_POST['it-exchange-modify-default-redirects'] ) || empty( $_GET['add-on-settings'] ) || 'modify-default-redirects' != $_GET['add-on-settings'] )
+        return;
+
+	// Loop through array, validate, save anything that isn't default
+	$redirects = array();
+	foreach( (array) $_POST['it-exchange-modify-default-redirects'] as $key => $values ) {
+		$type = empty( $values['type'] ) ? false : $values['type'];
+		if ( empty( $type ) || empty( $values[$type] ) )
+			continue;
+
+		$redirects[$key] = array( 'type' => $type, 'target' => $values[$type] );
+	}
+
+	// Save data
+    it_exchange_save_option( 'addon_modify_default_redirects', $redirects );
+}
+add_action( 'admin_init', 'it_exchange_modify_default_redirects_save_settings' );
